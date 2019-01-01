ec307dff79c11b4dff28ab04154af5fd5605fa82
sleepingkirby@gmail.com <sleepingkirby@gmail.com>
2019-01-01 19:24:37 +0800

    fixed a bug and also added exception for chrome:// pages

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(function() {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {urlMatches: '[a-z]*'},
+        pageUrl: {urlMatches: '(http|https|file)://+[a-z]*'},
       })],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
