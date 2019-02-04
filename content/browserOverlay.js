478bdd5ff5b05def94ecc977cfb012a9f3cf8f7a
sleepingkirby@gmail.com <sleepingkirby@gmail.com>
2019-02-04 20:30:15 +0800

    starting settings menu

--- content/browserOverlay.js
+++ content/browserOverlay.js
@@ -5,21 +5,39 @@
 //Components.classes["@mozilla.org/consoleservice;1"].getService(Components.interfaces.nsIConsoleService).logStringMessage("test");
 if ("undefined" == typeof(butWhyMod)) {
   var butWhyMod = {
-
+  curWin: gBrowser.getBrowserForTab(gBrowser.selectedTab),
     init : function(){
-    console.log("==============>> on init");
-    console.log(document.documentElement.innerHTML);
-    },
-    sayHello : function(aEvent) {
+    //get settings
+    //if no settings exist, set defaults.
+    console.log("butWhyMod: Starting butWhyMod.");
     }
-  }
+  };
 
  butWhyMod.init();
 };
 
+
+butWhyMod.curWin.addEventListener("complete", function (){
+
+    console.log("==============>> on event");
+});
+
+
+/*
 gBrowser.addEventListener("load", function () {
     console.log("==============>> on event");
-    console.log(document.documentElement.innerHTML);
-    console.log(gBrowser.selectedTab.body.innerHTML);
+// Add tab, then make active
+//gBrowser.selectedTab = gBrowser.addTab("http://www.google.com/");
+
+  var newTabBrowser = gBrowser.getBrowserForTab(gBrowser.selectedTab);
+
+  newTabBrowser.addEventListener("complete", function (){
+    console.log(newTabBrowser.contentDocument.readyState);
+    console.log(newTabBrowser.contentDocument.documentElement.innerHTML);
+    newTabBrowser.contentDocument.documentElement.setAttribute('style', 'border: 9px solid blue;');
+  }, true);
+
+
 }, true);
+*/
 
